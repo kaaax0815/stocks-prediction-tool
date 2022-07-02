1fafdee57b4bdf1000dcd773303b98b7f737f280
kaaax0815 <999999bst@gmail.com>
2022-07-02 17:09:44 +0200

    CapRover & Docker Support
    Squashed commit of the following:
    
    commit 25db3500b0ae5a09e9c7fe5aeb553f8548936f34
    Author: kaaax0815 <999999bst@gmail.com>
    Date:   Sat Jul 2 16:51:02 2022 +0200
    
        load env var at build time
    
    commit f5195577de2c6e62fe5797e44b7e87774717dac9
    Author: kaaax0815 <999999bst@gmail.com>
    Date:   Sat Jul 2 16:34:55 2022 +0200
    
        Delete env file
    
    commit 26e7d5436ff815d1959ccf439dee044a2f298d84
    Author: kaaax0815 <999999bst@gmail.com>
    Date:   Sat Jul 2 16:30:49 2022 +0200
    
        dynamicly specify backend url
    
    commit b12002d8614a30084dae25b7c484dec8218f1f18
    Author: kaaax0815 <999999bst@gmail.com>
    Date:   Sat Jul 2 00:42:46 2022 +0200
    
        frontend typescript wont be installed
    
    commit 88f0c6679f913157ae4d2c386b06293dec93f997
    Author: kaaax0815 <999999bst@gmail.com>
    Date:   Sat Jul 2 00:39:47 2022 +0200
    
        fix frontend script
    
    commit 273fc1ba1cabef71daa2ac61db561be66d474ffd
    Author: kaaax0815 <999999bst@gmail.com>
    Date:   Sat Jul 2 00:38:20 2022 +0200
    
        new frontend docker script
    
    commit b3424561697b17e6fb0999593596bed747201e53
    Author: kaaax0815 <999999bst@gmail.com>
    Date:   Sat Jul 2 00:30:09 2022 +0200
    
        build nextjs without standalone output
    
    commit 765ac41a565ee43b7aa3cdcbeedad0c477a11fde
    Author: kaaax0815 <999999bst@gmail.com>
    Date:   Sat Jul 2 00:24:56 2022 +0200
    
        Revert "export all necessary files to one directory"
    
        This reverts commit 73c156fbb2589692f6a4ec9afc9bc955a845fa6c.
    
    commit 73c156fbb2589692f6a4ec9afc9bc955a845fa6c
    Author: kaaax0815 <999999bst@gmail.com>
    Date:   Sat Jul 2 00:02:48 2022 +0200
    
        export all necessary files to one directory
    
    commit 6133d981b3045ec9e7d3031509b7f1d52ea845ad
    Author: kaaax0815 <999999bst@gmail.com>
    Date:   Sat Jul 2 00:00:14 2022 +0200
    
        make backend accessibly
    
    commit a96fbb9b83afd822a9d759471faae522274d9595
    Author: kaaax0815 <999999bst@gmail.com>
    Date:   Fri Jul 1 23:56:07 2022 +0200
    
        add dockerfiles
    
    commit c72ec5014e56f116c8a9598ebf1a2957717f23a7
    Author: kaaax0815 <999999bst@gmail.com>
    Date:   Fri Jul 1 23:51:00 2022 +0200
    
        fix spelling mistake
    
    commit eb5f1e67374093be4e393ea45cd8150b896c0ee0
    Author: kaaax0815 <999999bst@gmail.com>
    Date:   Fri Jul 1 23:48:01 2022 +0200
    
        test deployment with caprover

--- frontend/util/api.ts
+++ frontend/util/api.ts
@@ -1,6 +1,6 @@
 import { dateToTimestamp, subtractYears } from './date';
 
-const API_URL = 'http://127.0.0.1:5000';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:5000';
 
 export async function getBars(stock: string): Promise<Bar[]> {
   const to = dateToTimestamp(new Date());
